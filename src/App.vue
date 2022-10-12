d30c787b2da71dccc9678c56168e10140e725164
Tia <nik12346@yandex.ru>
2022-10-12 18:05:54 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="post">
+        <div><strong>Название: </strong>Пост о JavaScript</div>
+        <div><strong>Описание: </strong>JavaScript универсальный язык программирования</div>
+    </div>
+</template>
+
+<script>
+export default {  
+
+};
+</script>
+
+<style>
+
+</style>
