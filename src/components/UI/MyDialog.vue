dfdecc537ba616ac58cb12f5ef94b868b684f783
Tia <nik12346@yandex.ru>
2023-01-01 17:18:19 +0300

    Revert "add mixin"
    This reverts commit f8be42409fc46ce2af9f5b708da02fda7b6b86d7.

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -7,11 +7,21 @@
 </template>
 
 <script>
-import toggleMixin from '@/mixins/toggleMixin';
-
     export default {
         name: "my-dialog",
-        mixins: [toggleMixin]
+
+        props: {
+            show:{
+                type: Boolean,
+                default: false
+            }
+        },
+
+        methods:{
+            hideDialog(){
+                this.$emit("update:show", false);
+            }
+        }
     }
 </script>
 
